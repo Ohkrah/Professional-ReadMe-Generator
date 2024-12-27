fadfbda69dbb59792c0050dc463c2a2f261712f1
Ohkrah <dion.cobb.thornhill@gmail.com>
2024-12-27 11:46:34 -0800

    added questions, and started working on generator functions

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description',
+    },
+    {
+        type: 'input',
+        name: 'intallation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What steps are required in order for your project to function?',
+    },
+    {
+        type: 'input',
+        name: 'feature',
+        message: 'What are the features of your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you run tests for your application',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'motive',
+        message: 'What was your motivation?',
+    },
+    {
+        type: 'input',
+        name: 'reasoning',
+        message: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve?',
+    },
+    {
+        type: 'input',
+        name: 'learnings',
+        message: 'What did you learn?',
+    },
+    {
+        type: 'input',
+        name: 'unique',
+        message: 'What makes your project stand out?',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const fileContent = generateMarkdown(answers);
+
+        fs.writeFile('README.md', fileContent, (err) => 
+            err ? console.log(err) : console.log('README.md was successfully created')    
+        );
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license){
+    return '';
+  }else{
+    
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {if (!license){
+  return '';
+}else{
+  
+}}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license){
+    return '';
+  }else{
+    
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+   ${data.description}
+  
+  ## How to install
+  ${data.installation}
+
+  ## How to
+  ${data.usage}
+
+  ## Features
+  ${data.features}
+
+  ## How can tests be run on this app?
+  ${data.tests}
+
+  ## How can others contribute?
+  ${data.contribution}
+
+  ## Motive for development
+  ${data.motive}
+
+  ## Reason for development
+  ${data.reasoning}
+
+  ## What problems does this application solve
+  ${data.problem}
+
+  ## What was learned through creating this application
+  ${data.learnings}
+
+  ## Things unique to this project
+  ${data}
+
+
 
 `;
 }
